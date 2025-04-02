e5aae8c22efa7dbc780ae75c24fae97442b07965
Ben <98676125+B3ngineering@users.noreply.github.com>
2025-04-02 18:31:13 -0300

    Fixed spacing caused by transfer from Coderbyte

--- main.cpp
+++ main.cpp
@@ -5,9 +5,7 @@
 #include <iomanip>
 #include <mutex>
 
-
 using namespace std;
-
 
 // Create a structure to hold the data for each thread to make its calculation
 struct ThreadData {
@@ -19,27 +17,21 @@ struct ThreadData {
     mutex* mtx;
 };
 
-
-
-
 // Function to compute the element of the result matrix
 void calculateElement(ThreadData data) {
     int a = data.A->size();
     int b = data.B->size();
     int sum = 0;
 
-
     // Multiply row A by column B
     for (int i = 0; i < a; ++i) {
         sum += (*data.A)[data.row][i] * (*data.B)[i][data.col];
     }
 
-
     // Lock the mutex to safely update the result matrix
     lock_guard<mutex> lock(*data.mtx);
     (*data.C)[data.row][data.col] = sum;
 }
-
 
 // Function to print the matrix
 void printMatrix(const vector<vector<int>>& matrix) {
@@ -52,13 +44,11 @@ void printMatrix(const vector<vector<int>>& matrix) {
     cout << endl;
 }
 
-
 int main() {
     // Get matrix parameters
     int p, q, r;
     std::cout << "Enter the dimensions of the matrices (p, q, r): ";
     std::cin >> p >> q >> r;
-
 
     // End program if input is invalid
     if (p >= 100 || q >= 100 || r >= 100) {
@@ -66,12 +56,10 @@ int main() {
         return 1;
     }
 
-
     // Create random generator for 0-9
     random_device rd;
     mt19937 gen(rd());
     uniform_int_distribution<> dis(0, 9);
-
 
     // Create matrix A (p x q)
     vector<vector<int>> A(p, vector<int>(q));
@@ -80,7 +68,6 @@ int main() {
             A[i][j] = dis(gen);
         }
     }
-
 
     // Create matrix B (q x r)
     vector<vector<int>> B(q, vector<int>(r));
@@ -93,10 +80,8 @@ int main() {
     // Create matrix C (p x r) with all zeroes
     vector<vector<int>> C(p, vector<int>(r, 0));
 
-
     vector<thread> threads;
     mutex mtx;
-
 
     // Spawn threads for each element and execute at once
     for (int i = 0; i < p; i++) {
@@ -106,20 +91,16 @@ int main() {
         }
     }
 
-
     // Wait for all threads to finish to avoid conflicts
     for (auto& t : threads) {
         t.join();
     }
 
-
     cout << "Matrix A:" << endl;
     printMatrix(A);
 
-
     cout << "Matrix B:" << endl;
     printMatrix(B);
-
 
     cout << "Matrix C (Result):" << endl;
     printMatrix(C);
